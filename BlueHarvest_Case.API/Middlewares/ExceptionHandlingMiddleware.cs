9b4ba32d056fd091d09a373f32bc17059f08c550
cTarkan <cenkhantarkan@hotmail.com>
2025-01-29 23:57:45 +0300

    Exception middleware is added

---
+++ BlueHarvest_Case.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿namespace BlueHarvest_Case.API.Middlewares
+{
+	public class ExceptionHandlingMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public ExceptionHandlingMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception ex)
+			{
+				await HandleGenericExceptionAsync(context, ex);
+			}
+		}
+
+		private Task HandleGenericExceptionAsync(HttpContext context, Exception exception)
+		{
+			context.Response.ContentType = "application/json";
+			context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+			var errorDetails = new
+			{
+				Message = "An unexpected error occurred",
+				Error = exception.Message
+			};
+
+			return context.Response.WriteAsJsonAsync(errorDetails);
+		}
+	}
+}
